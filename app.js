dcfb17367d917976c6c6fa13753739ec6580f3e3
Michaela Satarova <ms@binaryone.ch>
2021-10-13 15:51:18 +0200

    better structure

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-let express = require('express')
-const { MongoClient, ObjectId } = require("mongodb");
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require ('connect-mongo');
+const flash = require('connect-flash')
+const app = express()
 
-let app = express()
-let db
-
-app.use(express.static('public'))
-
-let connectionString = 'mongodb+srv://todoappuser:Miminka123@cluster0.2hvch.mongodb.net/TaskManagemant?retryWrites=true&w=majority'
-MongoClient.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
-  db = client.db()
-  app.listen(3000)
+let sessionOptions = session({
+    secret: "Javascript is cool",
+    resave: false,
+    store: MongoStore.create({client: require('./db')}),
+    saveUninitialized: false,
+    cookie:{
+        maxAge: 1000 * 60 *60*24, 
+        httpOnly:true
+    }
 })
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+const router = require('./router');
 
+app.use(sessionOptions)
+app.use(flash())
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use(function(req, res, next){
+    res.locals.user = req.session.user
+    next()
+})
+
+//sem nastviš folder kde su uložene všetky tvoje css img a ine subory
+app.use(express.static('public'))
 // to prve musi byt views to druhe je meno foldra kde  mas templates
 app.set('views', 'views')
 // to prve musi byt views engine to druhe je meno prípony pre files
 app.set('view engine', 'ejs')
 
-app.get('/', function(req, res) {
-    res.render('index')
-})
 
-//ADD Project
-app.post('/create-project', function(req, res) {
-    db.collection('projects').insertOne({
-        project_name: req.body.project_name,
-        project_description: req.body.project_description,
-        deadline: req.body.deadline,
-        coder: req.body.coder,   
-        status: req.body.status   
-        }, function() {
-      /* res.redirect('/') */
-      res.send("thank you")
-      console.log(req.body.project_name)
-    })
-  })
+app.use('/', router)
+//totot spojenie mame v db.js ze pocuva na ktorom porte
+module.exports = app
